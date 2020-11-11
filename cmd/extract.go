04e1104c69b41ca63fb78cad1e26c9eabdaee439
x0r19x91 <x0r19x91@outlook.com>
2020-11-11 14:36:56 +0530

    Fix String parsing

--- cmd/extract.go
+++ cmd/extract.go
@@ -54,7 +54,7 @@ func NewIndentOptions(rd string) *indentOptions {
         useTabs:            false,
         caseMap:            tidy.AutoDetect,
         useEndFuncComments: true,
-        nMaxStrLitSize:     80,
+        nMaxStrLitSize:     160,
         useExtraNewline:    true,
     }
     data := make(map[string]string)
